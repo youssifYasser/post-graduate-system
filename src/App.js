7be03d0cd9939261c301e700c6b7c2ae495f0069
youssef yasser <youssifyasserme1999@gmail.com>
2021-01-24 23:21:22 +0200

    undo the testing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        Post graduate system
-      </div>
-    );
+    return <div className='App'>Post graduate system</div>
   }
 }
 
-export default App;
+export default App
