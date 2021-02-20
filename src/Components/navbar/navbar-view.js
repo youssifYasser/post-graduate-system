8cfb18fa90e1d16e352309cf0b574263a805787c
youssef yasser <youssifyasserme1999@gmail.com>
2021-02-21 00:23:30 +0200

    changed homePage to routing

--- src/Components/navbar/navbar-view.js
+++ src/Components/navbar/navbar-view.js
@@ -1,33 +1,41 @@
 import React from 'react'
-import { Link, Switch } from 'react-router-dom';
-import { Navbar, Nav, Container } from "react-bootstrap";
+import { Link, Switch } from 'react-router-dom'
+import { Navbar, Nav, Container } from 'react-bootstrap'
 
 import './navbar-style.css'
 import logo from './logo.png'
 
 function navbar() {
-    return (
-        <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-            <Container>
-                <Navbar.Collapse id="responsive-navbar-nav">
-                    <Nav>
-                        <Link to="/AddStudent" className="link"> إضافة طالب جديد </Link>
-                        <Link to="/StudentData" className="link"> تسجيل بيانات الطالب</Link>
-                    </Nav>
-                </Navbar.Collapse>
+  return (
+    <Navbar collapseOnSelect expand='lg' bg='dark' variant='dark'>
+      <Container>
+        <Navbar.Collapse id='responsive-navbar-nav'>
+          <Nav>
+            <Link to='/AddStudent' className='link'>
+              {' '}
+              إضافة طالب جديد{' '}
+            </Link>
+            <Link to='/StudentData' className='link'>
+              {' '}
+              تسجيل بيانات الطالب
+            </Link>
+          </Nav>
+        </Navbar.Collapse>
 
-                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-                <Navbar.Brand href="/">
-                    <img
-                        src={logo}
-                        width="250"
-                        className="d-inline-block align-top"
-                        alt="logo"
-                    />
-                </Navbar.Brand>
-            </Container>
-        </Navbar>
-    )
+        <Navbar.Toggle aria-controls='responsive-navbar-nav' />
+        <Link to='/' exact>
+          <Navbar.Brand>
+            <img
+              src={logo}
+              width='250'
+              className='d-inline-block align-top'
+              alt='logo'
+            />
+          </Navbar.Brand>
+        </Link>
+      </Container>
+    </Navbar>
+  )
 }
 
-export default navbar;
+export default navbar
