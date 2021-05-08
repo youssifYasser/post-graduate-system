d6ee01f7505c864f5494ef44db011d9eea261c4b
youssef yasser <youssifyasserme1999@gmail.com>
2021-05-08 09:43:13 +0200

    edited the sentence

--- src/Components/view-departments/no-departments.js
+++ src/Components/view-departments/no-departments.js
@@ -5,7 +5,7 @@ import './viewDepartments-style.css'
 const NoDepartments = () => {
   return (
     <div className='no-items'>
-      <h2>لا يوجد أقسام للعرض</h2>
+      <h2>لا يوجد أقسام للعرض...</h2>
     </div>
   )
 }
