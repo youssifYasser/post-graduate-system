4f1f7b67d221c72545ebf80e050b4876c0c15835
MostafaAmrHamed <mostafaamro5619@gmail.com>
2021-05-29 00:46:05 +0200

    Template designed

--- src/Components/home-page/homePage-view.js
+++ src/Components/home-page/homePage-view.js
@@ -1,65 +1,172 @@
 import React from 'react'
 import { Container, Image, Row, Col } from 'react-bootstrap'
 
-import maintenance from './image/maintenance.png'
+import { FaUserGraduate, FaEdit } from 'react-icons/fa'
+import { MdAddCircle } from 'react-icons/md'
+import { GiHandOk } from 'react-icons/gi'
+import { RiFileExcel2Line } from 'react-icons/ri'
+import { AiOutlineEye } from 'react-icons/ai'
+import { HiOutlineLightBulb } from 'react-icons/hi'
+
+import 'animate.css/animate.min.css'
+
+import cover from './image/AinShamss.jpg'
+import students from './image/students.jpg'
 
 import './homePage-style.css'
 
 function homePage() {
     return (
-        <div className="home text-center">
-            {/* <Container>
-                <h1> Home Page Content</h1>
-                <Carousel>
-                    <Carousel.Item>
-                        <img
-                            className="d-block w-100"
-                            src={bg1}
-                            alt="First slide"
-                            height="500" width="200"
-                        />
-                        <Carousel.Caption>
-                            <h3>Faculty of Science- Ain Shams University</h3>
-                            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-                        </Carousel.Caption>
-                    </Carousel.Item>
-                    <Carousel.Item>
-                        <img
-                            className="d-block w-100"
-                            src={bg2}
-                            alt="Second slide"
-                            height="500" width="200"
-                        />
+        <div className="home">
+            <div className="cover mb-5">
+                <Image src={cover} width={'100%'} alt="Ain Shams" className="image" />
+            </div>
 
-                        <Carousel.Caption>
-                            <h3>Faculty of Science- Ain Shams University</h3>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                        </Carousel.Caption>
-                    </Carousel.Item>
-                </Carousel>
-            </Container> */}
-
-            {/* <img src={img} alt=""/> */}
-
-            {/* <Container>
+            <Container className="about text-center ">
                 <Row>
-                    <Col xs="12" md="4" >
-                        <h1>تحت الصيانة</h1>
+                    <Col>
+                        <div className="bg-icon animate__animated animate__backInLeft">
+                            <HiOutlineLightBulb className="icon" />
+                        </div>
                     </Col>
-                    <Col xs="12" md="4" >
-
+                    <Col>
+                        <div className="bg-icon animate__animated animate__backInRight">
+                            <AiOutlineEye className="icon" />
+                        </div>
                     </Col>
                 </Row>
-            </Container> */}
-            <Image
-                className="d-block mx-auto img-fluid w-5"
-                src={maintenance}
-                alt="Coming soon"
-                width="500"
-                height="200"
-            />
-            <h1 className="mb-5" style={{color:"red"}}> الصفحة التى تحاول الوصول إليها تحت الصيانة حالياََ </h1>
-            <h1 style={{color:"red"}}> The page you're looking for is under maintenance </h1>
+                <Row>
+                    <Col>
+                        <h2 className="title">مهمتنا</h2>
+                    </Col>
+                    <Col>
+                        <h2 className="title">اى حاجه</h2>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit.<br />
+                            Id doloremque amet accusamus assumenda atque repellendus quasi,<br />
+                            corrupti repellat dolores a officiis?<br />
+                            Iusto obcaecati itaque totam ea facere esse. <br />
+                            Blanditiis, nulla.
+                        </p>
+                    </Col>
+                    <Col>
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit.<br />
+                            Id doloremque amet accusamus assumenda atque repellendus quasi,<br />
+                            corrupti repellat dolores a officiis?<br />
+                            Iusto obcaecati itaque totam ea facere esse. <br />
+                            Blanditiis, nulla.
+                        </p>
+                    </Col>
+                </Row>
+            </Container>
+
+            <div className=" section mb-5">
+                <Row>
+                    <Col md={6}>
+                        <div className="content">
+                            <p className="title">الطلاب</p>
+                            <div className="line"></div>
+                            <div className="circle">
+                                <p className="icon"><FaUserGraduate /></p>
+                                <h2 className="count">22,561</h2>
+                                <h3>عدد الطلاب</h3>
+                            </div>
+
+                            <div className="links mt-5">
+                                <Row className="row-1">
+                                    <Col lg={12} xl={{ span: 5, offset: 2 }}>
+                                        <div className="link">
+                                            <Row>
+                                                <Col>
+                                                    <p className="textOflink text-center">إضافة طالب</p>
+                                                </Col>
+                                                <Col md={3}>
+                                                    <MdAddCircle className="icon2" />
+                                                </Col>
+                                            </Row>
+                                        </div>
+                                    </Col>
+                                    <Col lg={12} xl={5}>
+                                        <div className="link">
+                                            <Row>
+                                                <Col>
+                                                    <p className="textOflink text-center">عرض / بحث/ تعديل</p>
+                                                </Col>
+                                                <Col md={4}>
+                                                    <FaEdit className="icon2" />
+                                                </Col>
+                                            </Row>
+                                        </div>
+                                    </Col>
+                                </Row>
+                                <Row>
+                                    <Col lg={12} xl={{ span: 5, offset: 2 }}>
+                                        <div className="link">
+                                            <Row>
+                                                <Col>
+                                                    <p className="textOflink text-center"> تسجيل بيانات يدويا</p>
+                                                </Col>
+                                                <Col md={3}>
+                                                    <GiHandOk className="icon2" />
+                                                </Col>
+                                            </Row>
+                                        </div>
+                                    </Col>
+                                    <Col lg={12} xl={5}>
+                                        <div className="link">
+                                            <Row>
+                                                <Col>
+                                                    <p className="textOflink text-center">تسجيل بيانات بالإكسل</p>
+                                                </Col>
+                                                <Col md={3}>
+                                                    <RiFileExcel2Line className="icon2" />
+                                                </Col>
+                                            </Row>
+                                        </div>
+                                    </Col>
+                                </Row>
+                            </div>
+                        </div>
+                    </Col>
+                    <Col md={6}>
+                        <img src={students} alt="Students" width={'100%'} height={800} className="image" />
+                    </Col>
+                </Row>
+            </div>
+            <Container className="stats mb-5">
+                <h1 className="mb-5">عدد الطلاب فى كل دراسة</h1>
+                <Row>
+                    <Col>
+                        <div className="circle">
+                            <h2 className="count">561</h2>
+                            <h3>دبلومة الدراسات العليا</h3>
+                        </div>
+                    </Col>
+                    <Col>
+                        <div className="circle">
+                            <h2 className="count">2,212</h2>
+                            <h3>تمهيدي الماجستير</h3>
+                        </div>
+                    </Col>
+                    <Col>
+                        <div className="circle">
+                            <h2 className="count">14,154</h2>
+                            <h3>الماجستير في العلوم</h3>
+                        </div>
+                    </Col>
+                    <Col>
+                        <div className="circle">
+                            <h2 className="count">1,981</h2>
+                            <h3>دكتوراه الفلسفة في العلوم</h3>
+                        </div>
+                    </Col>
+                </Row>
+            </Container>
         </div>
 
     )
